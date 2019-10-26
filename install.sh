7875efc68b33afa373e843426311d693e28bd10f
Frederic Depuydt <f.depuydt@outlook.com>
2019-10-26 21:42:42 +0100

    Cleaning up install and start script

--- install.sh
+++ install.sh
@@ -11,7 +11,10 @@
 . /usr/local/lib/depuydt/sh/echoes.sh
 
 ## TITLE
-echo_section "DOCKER DEPLOYING:" "Automation Stack"
+echo_section "DOCKER DEPLOYING:" "Automation Stack (Installing)"
+
+# Creating external networks
+if [ -z "$(docker network list -f name=^web$ -q)" ]; then docker network create web; fi
 
 ## Creating the volumes, networks and containers
 docker-compose up --build --no-start
